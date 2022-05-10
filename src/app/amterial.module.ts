90b89e70819463f8bc8be5ccb17de1493824e206
Monsingozzy <mohansinghyoyo88@gmail.com>
2022-05-10 17:30:40 +0530

    first one

---
+++ src/app/amterial.module.ts
@@ -0,0 +1,37 @@
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from "@angular/core";
+import { MatSliderModule} from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+    imports:[MatSliderModule,
+             MatButtonModule,
+             MatIconModule,
+             MatFormFieldModule,
+             MatInputModule,
+             MatDatepickerModule,
+             MatNativeDateModule,
+             MatCheckboxModule
+            
+             
+            ],
+    exports:[MatSliderModule,
+             MatButtonModule,
+             MatIconModule,
+             MatFormFieldModule,
+             MatInputModule,
+             MatDatepickerModule,
+             MatNativeDateModule,
+             MatCheckboxModule
+            ]
+    
+})
+
+
+export class MaterialModule{}
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+maxdate: Date;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.maxdate=new Date();
+    this.maxdate.setFullYear(this.maxdate.getFullYear()-18)
+  }
+
+  onsubmit(form:NgForm){
+    console.log(form.value.Username);
+  }
+
+}
