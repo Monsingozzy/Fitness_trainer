0ff4cab0b3749fde5dbde9bc0d00f529e3b468ff
Monsingozzy <mohansinghyoyo88@gmail.com>
2022-05-14 13:45:40 +0530

    deplo

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,6 +8,7 @@ import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 @Output() sidenavToggle=new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
