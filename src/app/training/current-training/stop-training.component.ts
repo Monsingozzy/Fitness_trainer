1ac91fe6d4dde19c3b4e4951e90bb1af9829b397
Monsing <62444307+Monsingozzy@users.noreply.github.com>
2022-05-18 13:08:06 +0530

    Update stop-training.component.ts

--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -6,14 +6,11 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog'
   selector :'app-stop-training',
   template :`<p mat-dialog-title>Are you sure?</p>
    <p mat-dialog-title>You already completed {{passedData.process}}%</p>
-              <mat-dialog-actions>
-                <button mat-button [mat-dialog-close]="true">Yes</button>
-                <button mat-button [mat-dialog-close]="false">No</button>
-               </mat-dialog-actions>`
-
-
+    <mat-dialog-actions>
+        <button mat-button [mat-dialog-close]="true">Yes</button>
+        <button mat-button [mat-dialog-close]="false">No</button>
+    </mat-dialog-actions>`
 })
 export class StopTrainingComponent{
-
   constructor(@Inject(MAT_DIALOG_DATA) public passedData:any){}
 }
